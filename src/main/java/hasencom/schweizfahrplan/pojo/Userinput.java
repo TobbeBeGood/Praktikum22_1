d28400ffcbec0fec127444c1d9d66fc20ccccc58
treptowfelix <treptowfelix@gmail.com>
2022-02-28 13:33:48 +0100

    pojo updated

--- src/main/java/hasencom/schweizfahrplan/pojo/Userinput.java
+++ src/main/java/hasencom/schweizfahrplan/pojo/Userinput.java
@@ -1,5 +1,31 @@
 package hasencom.schweizfahrplan.pojo;
 
 
+import org.apache.catalina.User;
+
+import java.lang.reflect.Constructor;
+
 public class Userinput {
+    private String start_stadt;
+    private String ziel_stadt;
+
+    public void setStart_stadt(String start_stadt) {
+        this.start_stadt = start_stadt;
+    }
+    public void setZiel_stadt(String ziel_stadt) {
+        this.ziel_stadt = ziel_stadt;
+    }
+    public String getStart_stadt() {
+        return start_stadt;
+    }
+    public String getZiel_stadt() {
+        return ziel_stadt;
+    }
+
+    public Userinput(){
+    }
+    public Userinput(String start_stadt, String ziel_stadt){
+        this.start_stadt = start_stadt;
+        this.ziel_stadt = ziel_stadt;
+    }
 }
