18704ceab88dd79ed2c22cead608104893fa1731
treptowfelix <treptowfelix@gmail.com>
2022-02-28 09:51:14 +0100

    neue nachricht, start erfolgreich

--- src/main/java/hasencom/schweizfahrplan/SchweizfahrplanApplication.java
+++ src/main/java/hasencom/schweizfahrplan/SchweizfahrplanApplication.java
@@ -8,7 +8,7 @@ public class SchweizfahrplanApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchweizfahrplanApplication.class, args);
-		System.out.println("Moin Felix");
+		System.out.println("Start erfolgreich");
 
 	}
 
