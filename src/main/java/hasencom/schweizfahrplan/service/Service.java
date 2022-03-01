b74bd722b21e64f6c7f1228b9001623d7ca335a2
treptowfelix <treptowfelix@gmail.com>
2022-03-01 15:15:20 +0100

    pojo updated

--- src/main/java/hasencom/schweizfahrplan/service/Service.java
+++ src/main/java/hasencom/schweizfahrplan/service/Service.java
@@ -3,9 +3,17 @@ package hasencom.schweizfahrplan.service;
 import hasencom.schweizfahrplan.controller.Controller;
 import hasencom.schweizfahrplan.pojo.Userinput;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
 public class Service {
     public String getConnection(Userinput userinput){
-
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(
+                URI.create("http://transport.opendata.ch/v1/connections?from=Lausanne&to=Genève"))
+                .header("accept","applicatin/json")
+                .build();
         return "";
     }
     //Methode die HTTP GET Request an SChweiz API sendet ✘
