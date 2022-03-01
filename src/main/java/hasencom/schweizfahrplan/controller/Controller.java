cd3b4e6202711b7d6835f781820ca9d0058b30a7
treptowfelix <treptowfelix@gmail.com>
2022-03-01 11:03:57 +0100

    pojo updated

--- src/main/java/hasencom/schweizfahrplan/controller/Controller.java
+++ src/main/java/hasencom/schweizfahrplan/controller/Controller.java
@@ -1,8 +1,19 @@
 package hasencom.schweizfahrplan.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 public class Controller {
+    @GetMapping("/connection")
+    public String index() {
+        return "start";
+    }
+    @RequestMapping(value = "/connections",method = RequestMethod.GET)
+    public String windex() {
+        return "cool";
+    }
 
     //GET-Endpoint "/connection"
     //Requestparameter= Userinput
