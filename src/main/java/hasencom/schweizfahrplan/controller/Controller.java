80c4cebb1feb7940367628adc5a27ad8c18d8725
treptowfelix <treptowfelix@gmail.com>
2022-03-01 11:28:28 +0100

    pojo updated

--- src/main/java/hasencom/schweizfahrplan/controller/Controller.java
+++ src/main/java/hasencom/schweizfahrplan/controller/Controller.java
@@ -1,6 +1,7 @@
 package hasencom.schweizfahrplan.controller;
 
 
+import hasencom.schweizfahrplan.pojo.Userinput;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
