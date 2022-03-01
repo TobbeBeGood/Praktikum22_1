152c20bb33932648e06623be88ea916950f3eed8
treptowfelix <treptowfelix@gmail.com>
2022-03-01 12:13:08 +0100

    pojo updated

--- src/main/java/hasencom/schweizfahrplan/controller/Controller.java
+++ src/main/java/hasencom/schweizfahrplan/controller/Controller.java
@@ -2,18 +2,22 @@ package hasencom.schweizfahrplan.controller;
 
 
 import hasencom.schweizfahrplan.pojo.Userinput;
+import hasencom.schweizfahrplan.service.Service;
+import org.apache.catalina.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 public class Controller {
     @GetMapping("/connection")
-    public String index() {
-        return "start";
+    public String connection(@RequestParam Userinput userinput) {
+        Service serviceobjekt = new Service();
+        String speicher = serviceobjekt.getConnection(userinput);
+        return speicher;
     }
 
-    //GET-Endpoint "/connection"
-    //Requestparameter= Userinput
-    //Ruft Methode in Service auf
-
+    //GET-Endpoint "/connection" ✔
+    //Requestparameter= Userinput ✔
+    //Ruft Methode in Service auf ✔
 
 }
--- src/main/java/hasencom/schweizfahrplan/service/Service.java
+++ src/main/java/hasencom/schweizfahrplan/service/Service.java
@@ -1,12 +1,18 @@
 package hasencom.schweizfahrplan.service;
 
+import hasencom.schweizfahrplan.controller.Controller;
+import hasencom.schweizfahrplan.pojo.Userinput;
+
 public class Service {
+    public String getConnection(Userinput userinput){
 
-    //Methode die HTTP GET Request an SChweiz API sendet
-    // Parameter ist Eingabe des Nutzers (Userinput)
-    //GET http://transport.opendata.ch/v1/connections?from=Lausanne&to=Genève
-    //Ausgabe des zurückkommenden JSOn auf der Konsole
+        return "";
+    }
+    //Methode die HTTP GET Request an SChweiz API sendet ✘
+    // Parameter ist Eingabe des Nutzers (Userinput) ✔
+    //GET http://transport.opendata.ch/v1/connections?from=Lausanne&to=Genève ✘
+    //Ausgabe des zurückkommenden JSOn auf der Konsole ✘
 
-    //Methode, die die Ziel URL aus der API URL und den Eingaben des Nutzers zusammen setzt
+    //Methode, die die Ziel URL aus der API URL und den Eingaben des Nutzers zusammen setzt ✘
 
 }
