515707c6d44be808ed5d3fdd36ab818fa9c7c722
TorbenSchrötke <torben.schroetke@gmail.com>
2022-03-01 08:58:54 +0100

    KOmmentare

--- src/main/java/hasencom/schweizfahrplan/controller/Controller.java
+++ src/main/java/hasencom/schweizfahrplan/controller/Controller.java
@@ -1,6 +1,13 @@
 package hasencom.schweizfahrplan.controller;
 
+
+
 public class Controller {
+
+    //GET-Endpoint "/connection"
+    // Requestparameter= Userinput
+    //Ruft Methode in Service auf
+
 
 }
 
--- src/main/java/hasencom/schweizfahrplan/service/Service.java
+++ src/main/java/hasencom/schweizfahrplan/service/Service.java
@@ -2,4 +2,10 @@ package hasencom.schweizfahrplan.service;
 
 public class Service {
 
+    //Methode die HTTP GET Request an SChweiz API sendet
+    //GET http://transport.opendata.ch/v1/connections?from=Lausanne&to=Genève
+    //Ausgabe des zurückkommenden JSOn auf der Konsole
+
+    //Methode, die die Ziel URL aus der API URL und den Eingaben des Nutzers zusammen setzt
+
 }
