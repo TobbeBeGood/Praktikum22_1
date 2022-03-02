d6bbba784b846b7ff458399d6c22b83bbe6a9db6
treptowfelix <treptowfelix@gmail.com>
2022-03-02 09:01:32 +0100

    pojo updated

--- src/main/java/hasencom/schweizfahrplan/service/Service.java
+++ src/main/java/hasencom/schweizfahrplan/service/Service.java
@@ -7,6 +7,7 @@ import org.apache.catalina.User;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class Service {
     public String getConnection(Userinput userinput){
@@ -15,12 +16,13 @@ public class Service {
                 URI.create("http://transport.opendata.ch/v1/connections?from="+ userinput.getStart_stadt()+"to="+userinput.getZiel_stadt()))
                 .header("accept","application/json")
                 .build();
-        return "";
+        HttpResponse<String> response = client.send(request);
+        return response.body();
     }
 
-    //Methode die HTTP GET Request an SChweiz API sendet ✘
+    //Methode die HTTP GET Request an SChweiz API sendet ✔
     // Parameter ist Eingabe des Nutzers (Userinput) ✔
-    //GET http://transport.opendata.ch/v1/connections?from=Lausanne&to=Genève ✘
+    //GET http://transport.opendata.ch/v1/connections?from=Lausanne&to=Genève ✔
     //Ausgabe des zurückkommenden JSOn auf der Konsole ✘
 
     //Methode, die die Ziel URL aus der API URL und den Eingaben des Nutzers zusammen setzt ✘
