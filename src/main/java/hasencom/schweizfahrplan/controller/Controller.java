7607fd7405e039909ffe2c0924f2dba35fcbefc1
TorbenSchrötke <torben.schroetke@gmail.com>
2022-03-02 12:54:55 +0100

    HTTP request

--- src/main/java/hasencom/schweizfahrplan/controller/Controller.java
+++ src/main/java/hasencom/schweizfahrplan/controller/Controller.java
@@ -3,21 +3,25 @@ package hasencom.schweizfahrplan.controller;
 
 import hasencom.schweizfahrplan.pojo.Userinput;
 import hasencom.schweizfahrplan.service.Service;
-import org.apache.catalina.User;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@org.springframework.stereotype.Controller
 public class Controller {
     @GetMapping("/connection")
-    public String connection(@RequestParam Userinput userinput) {
-        Service serviceobjekt = new Service();
-        String speicher = serviceobjekt.getConnection(userinput);
-        return speicher;
+    public String index() throws Exception {
+        Service service = new Service();
+         String result=service.getConnection();
+
+        return result;
     }
 
-    //GET-Endpoint "/connection" ✔
-    //Requestparameter= Userinput ✔
-    //Ruft Methode in Service auf ✔
+
+    //GET-Endpoint "/connection"
+    //Requestparameter= Userinput
+    //Ruft Methode in Service auf
+
 
 }
--- src/main/java/hasencom/schweizfahrplan/service/Service.java
+++ src/main/java/hasencom/schweizfahrplan/service/Service.java
@@ -1,30 +1,68 @@
 package hasencom.schweizfahrplan.service;
 
-import hasencom.schweizfahrplan.controller.Controller;
-import hasencom.schweizfahrplan.pojo.Userinput;
-import org.apache.catalina.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+@org.springframework.stereotype.Service
 public class Service {
-    public String getConnection(Userinput userinput){
-        var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(
-                URI.create("http://transport.opendata.ch/v1/connections?from="+ userinput.getStart_stadt()+"to="+userinput.getZiel_stadt()))
-                .header("accept","application/json")
+
+
+   private static HttpURLConnection connection;
+    //Methode die HTTP GET Request an SChweiz API sendet
+    // Parameter ist Eingabe des Nutzers (Userinput)
+    //GET http://transport.opendata.ch/v1/connections?from=Lausanne&to=Genève
+    //Ausgabe des zurückkommenden JSOn auf der Konsole
+
+    //Methode, die die Ziel URL aus der API URL und den Eingaben des Nutzers zusammen setzt
+    public String getConnection() throws Exception {
+            BufferedReader reader;
+            String line;
+            StringBuffer responseContent = new StringBuffer();
+
+            try{
+                 URL url= new URL("http://transport.opendata.ch/v1/connections?from=Lausanne&to=Genève");
+                 connection = (HttpURLConnection) url.openConnection();
+
+                 connection.setRequestMethod("GET");
+                 connection.setConnectTimeout(5000);
+                 connection.setReadTimeout(5000);
+
+                 int status = connection.getResponseCode();
+                 //System.out.println(status);
+
+                reader = new BufferedReader(new InputStreamReader( (connection.getInputStream())));
+                while ((line=reader.readLine())!= null){
+                    responseContent.append(line);
+                }
+                reader.close();
+
+            }    catch (Exception e) {
+                }
+            connection.disconnect();
+            return responseContent.toString();
+        }
+
+       /* HttpClient client =HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://transport.opendata.ch/v1/connections?from=Lausanne&to=Genève"))
                 .build();
-        HttpResponse<String> response = client.send(request);
-        return response.body();
+       client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(System.out::println)
+                .join();*/
+
     }
 
-    //Methode die HTTP GET Request an SChweiz API sendet ✔
-    // Parameter ist Eingabe des Nutzers (Userinput) ✔
-    //GET http://transport.opendata.ch/v1/connections?from=Lausanne&to=Genève ✔
-    //Ausgabe des zurückkommenden JSOn auf der Konsole ✘
 
-    //Methode, die die Ziel URL aus der API URL und den Eingaben des Nutzers zusammen setzt ✘
 
-}
